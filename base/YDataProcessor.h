5022bec1ffa28846a53b8ee069c69e6e9139bee3
Yurter <sterben_idd@mail.ru>
2019-06-07 14:02:07 +0300

    [Temp]

--- base/YDataProcessor.h
+++ base/YDataProcessor.h
@@ -6,7 +6,7 @@
 #include "YAsyncQueue.h"
 
 template <class inType, class outType>
-class YDataProcessor
+class YDataProcessor : YThread
 {
 
 public:
@@ -16,8 +16,6 @@ public:
 
     virtual ~YDataProcessor()
     {
-        _input_thread.quit();
-        _output_thread.quit();
         _input_queue.clear();
     }
 
@@ -31,66 +29,33 @@ public:
         _input_queue.push(input_data);
     }
 
-    YCode start()
+protected:
+
+    virtual YCode processInputData(inType& input_data) = 0;
+    virtual YCode sendOutputData(outType& output_data) final
     {
         if (_next_processor == nullptr) {
             return YCode::NOT_INITED;
         }
-
-        _input_thread = [this](){
-            inType input_data;
-            if (!_input_queue.pop(input_data)) {
-                utils::sleep_for(SHORT_DELAY_MS);
-                return YCode::AGAIN;
-            }
-            auto ret = processInputData(input_data)
-        };
-
-        _output_thread = [this](){
-            //
-        };
-
-        _input_thread.start();
-        _output_thread.start();
-
-        if (!_input_thread.running() || !_output_thread.running()) {
-            _input_thread.quit();
-            _output_thread.quit();
-            return YCode::ERR;
-        }
+        _next_processor->push(output_data);
         return YCode::OK;
     }
-
-protected:
-
-    virtual YCode processInputData(inType& input_data, YDataProcessor* next_processor) = 0;
 
 private:
 
     YCode run() override final
     {
-        if (_next_processor == nullptr) {
-            return YCode::NOT_INITED;
-        }
         inType input_data;
         if (!_input_queue.pop(input_data)) {
             utils::sleep_for(SHORT_DELAY_MS);
             return YCode::AGAIN;
         }
-        outType output_data;
-        auto ret = processInputData(input_data, output_data);
-        if (!utils::exit_code(ret)) {
-            _next_processor->push(input_data);
-        }
-        return ret;
+        return processInputData(input_data);
     }
 
 private:
 
-    YThread                 _input_thread;
-    YThread                 _output_thread;
     YAsyncQueue<inType>     _input_queue;
-    YAsyncQueue<outType>    _output_queue;
     YDataProcessor*         _next_processor;
 
 };
