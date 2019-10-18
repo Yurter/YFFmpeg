af524d1658d76900213fd18914501cd04315bd6e
Yurter <sterben_idd@mail.ru>
2019-10-18 17:17:05 +0300

    перед переходом на смарт поинтеры

--- base/Data.hpp
+++ base/Data.hpp
@@ -1,3 +1,48 @@
+//#pragma once
+//#include "MediaData.hpp"
+//#include <memory>
+
+//namespace fpp {
+
+
+//    template <typename Type>
+//    class Data : public MediaData {
+
+//    public:
+
+//        Data() :
+//            Data(Type())
+//        {
+//            EMPTY_CONSTRUCTOR
+//        }
+
+////        Data(const Data&) = default;
+////        Data(Data&&) = default;
+
+//        Data(Type data, MediaType type = MediaType::MEDIA_TYPE_UNKNOWN) :
+//            MediaData(type),
+//            _data(data)
+//        {
+//            setName("Data");
+//        }
+
+//        virtual ~Data() override = default;
+
+//        void                setRaw(Type data) {
+//            _data.reset();
+//            _data = std::make_shared<Type>(data);
+//        }
+//        Type&               raw() { return _data.get(); }
+
+//    protected:
+
+//        std::shared_ptr<Type>   _data;
+
+//    };
+
+//} // namespace fpp
+
+
 #pragma once
 #include "MediaData.hpp"
 
