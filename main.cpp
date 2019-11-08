94cf747aadd81c95fdf8c869215a47204e4b76d5
Yurter <sterben_idd@mail.ru>
2019-11-08 18:56:51 +0300

    temp

--- main.cpp
+++ main.cpp
@@ -66,8 +66,8 @@ int main() {
         Pipeline pipeline;
         pipeline.addElement(source);
 
-        auto sink_event = new MediaSink("group_video/event.flv", IOType::Event);
-        pipeline.addElement(sink_event);
+//        auto sink_event = new MediaSink("group_video/event.flv", IOType::Event);
+//        pipeline.addElement(sink_event);
 
         auto video_params = new VideoParameters;
 //        video_params->setWidth(640);
@@ -80,8 +80,8 @@ int main() {
 //        auto sink_youtube = new MediaSink("group_video/youtube.flv", IOType::YouTube);
 //        pipeline.addElement(sink_youtube);
 
-        auto sink_timelapse = new MediaSink("group_video/timelapse.flv", IOType::Timelapse);
-        pipeline.addElement(sink_timelapse);
+//        auto sink_timelapse = new MediaSink("group_video/timelapse.flv", IOType::Timelapse);
+//        pipeline.addElement(sink_timelapse);
 
 //        auto sink_restream = new MediaSink("group_video/restream.flv");
 //        pipeline.addElement(sink_restream);
