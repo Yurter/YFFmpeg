2582aaa93b89d3076587ca39fd6b3ab7628b949b
Yurter <sterben_idd@mail.ru>
2019-11-21 14:02:05 +0300

    [Added] main.cpp bench.

--- main.cpp
+++ main.cpp
@@ -42,6 +42,75 @@ int main() {
 //        set_ffmpeg_log_level(LogLevel::Quiet);
 //        set_ffmpeg_log_level(LogLevel::Debug);
 
+        avdevice_register_all();
+        {
+            Pipeline* pipeline = new Pipeline;
+            if (auto ret = pipeline->start(); ret != Code::OK) {
+                static_log_error("main", "Pipeline start failed: " << ret << " - " << utils::code_to_string(ret));
+            }
+
+            auto source = new MediaSource("video=HP Wide Vision FHD Camera"); /* IP Camera */
+            source->setCloseOnDisconnect(false);
+            pipeline->addElement(source);
+
+
+            auto sink_event = new MediaSink("group_video/eventUSB.flv", IOType::Event);
+            pipeline->addElement(sink_event);
+
+            auto sink_timelapse = new MediaSink("group_video/timelapseUSB.flv", IOType::Timelapse);
+            pipeline->addElement(sink_timelapse);
+
+            auto params = new VideoParameters;
+            params->setStreamIndex(0);
+            auto custom_sink = new CustomPacketSink {
+                        "test"
+                        , { new VideoStream(params) }
+                        , [](Packet& packet) {
+                            UNUSED(packet);
+                            return Code::OK;
+                        }
+                        , [](Packet& packet) {
+                            UNUSED(packet);
+                            return Code::OK;
+                        }
+            };
+            pipeline->addElement(custom_sink);
+        }
+        {
+            Pipeline* pipeline = new Pipeline;
+            if (auto ret = pipeline->start(); ret != Code::OK) {
+                static_log_error("main", "Pipeline start failed: " << ret << " - " << utils::code_to_string(ret));
+            }
+
+            auto source = new MediaSource("rtsp://admin:admin@192.168.10.3"); /* IP Camera */
+            source->setCloseOnDisconnect(false);
+            pipeline->addElement(source);
+
+
+            auto sink_event = new MediaSink("group_video/eventIP.flv", IOType::Event);
+            pipeline->addElement(sink_event);
+
+            auto sink_timelapse = new MediaSink("group_video/timelapseIP.flv", IOType::Timelapse);
+            pipeline->addElement(sink_timelapse);
+
+            auto params = new VideoParameters;
+            params->setStreamIndex(0);
+            auto custom_sink = new CustomPacketSink {
+                        "test"
+                        , { new VideoStream(params) }
+                        , [](Packet& packet) {
+                            UNUSED(packet);
+                            return Code::OK;
+                        }
+                        , [](Packet& packet) {
+                            UNUSED(packet);
+                            return Code::OK;
+                        }
+            };
+            pipeline->addElement(custom_sink);
+        }
+        utils::sleep_for_min(60);
+
         /* Запись rtsp с камеры в flv/YouTube */
 //        std::string mrl_src = "camera_one_hour_realtime.flv";
 //        std::string mrl_src = "camera_video_10.flv";
@@ -49,8 +118,9 @@ int main() {
 //        std::string mrl_src = "big_buck_bunny.mp4";
 //        std::string mrl_src = "Walking.mp4"; /* Не работает декодер! */
 //        std::string mrl_src = "rtsp://admin:Admin2019@192.168.10.12"; //640x480
-        std::string mrl_src = "rtsp://admin:admin@192.168.10.3";
-//        std::string mrl_src = "video=HP Wide Vision FHD Camera";
+//        std::string mrl_src = "rtsp://admin:admin@192.168.10.3";
+        avdevice_register_all();
+        std::string mrl_src = "video=HP Wide Vision FHD Camera";
 
 //        std::string mrl_dst = "rtmp://a.rtmp.youtube.com/live2/vtpz-spss-u4eq-4k0e";
     //    std::string mrl_dst = "remuxed.flv";
@@ -83,8 +153,6 @@ int main() {
 //        auto sink_youtube = new MediaSink("group_video/youtube.flv", IOType::YouTube);
 //        pipeline.addElement(sink_youtube);
 
-//        auto sink_timelapse = new MediaSink("group_video/timelapse.flv", IOType::Timelapse);
-//        pipeline.addElement(sink_timelapse);
 
 //        auto sink_restream = new MediaSink("group_video/restream.flv");
 //        pipeline.addElement(sink_restream);
@@ -95,21 +163,26 @@ int main() {
             static_log_error("main", "Pipeline start failed: " << ret << " - " << utils::code_to_string(ret));
         }
 
-        auto params = new VideoParameters;
-        params->setStreamIndex(0);
-        auto custom_sink = new CustomPacketSink {
-                    "test"
-                    , { new VideoStream(params) }
-                    , [](Packet& packet) {
-                        std::cout << "write_func: " << packet << std::endl;
-                        return Code::OK;
-                    }
-                    , [](Packet& packet) {
-                        std::cout << "process_func: " << packet << std::endl;
-                        return Code::OK;
-                    }
-        };
-        pipeline.addElement(custom_sink);
+
+        auto sink_timelapse = new MediaSink("group_video/timelapse.flv", IOType::Timelapse);
+        pipeline.addElement(sink_timelapse);
+
+//        auto params = new VideoParameters;
+//        params->setStreamIndex(0);
+////        params->setWidth(2000);
+//        auto custom_sink = new CustomPacketSink {
+//                    "test"
+//                    , { new VideoStream(params) }
+//                    , [](Packet& packet) {
+////                        std::cout << "write_func: " << packet << std::endl;
+//                        return Code::OK;
+//                    }
+//                    , [](Packet& packet) {
+////                        std::cout << "process_func: " << packet << std::endl;
+//                        return Code::OK;
+//                    }
+//        };
+//        pipeline.addElement(custom_sink);
 
 //        std::thread([&pipeline](){
 //            for (int i = 0; i < 60; i++) {
