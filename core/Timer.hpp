d0081275cd806f48e52b08da6d26d4e9100da9c8
Yurter <sterben_idd@mail.ru>
2019-12-05 11:41:16 +0300

    tmp

--- core/Timer.hpp
+++ core/Timer.hpp
@@ -1,41 +1,69 @@
 #pragma once
 #include <functional>
+#include <thread>
+#include <chrono>
+#include "AsyncQueue.hpp"
 
 namespace fpp {
 
-    template<typename FunctionType>
+//    using DelayedFunction = std::function<void()>;
+//    using ExecuteTime = std::chrono::time_point<std::chrono::steady_clock>;
+//    using ScheduledFunction = std::pair<DelayedFunction, ExecuteTime>;
+
+    struct DelayedFunction {
+        std::function<void()>   function;
+        int                     delay;
+    };
+
     class Timer {
 
     public:
 
-        Timer(FunctionType function) {
-            // TODO
+        Timer() {
+            _thread = std::thread([&]() {
+                DelayedFunction delayed_function;
+                while (_function_queue.wait_and_pop(delayed_function)) {
+                    std::this_thread::sleep_for(
+                        std::chrono::milliseconds(delayed_function.delay)
+                    );
+                }
+            });
         }
+
         ~Timer() {
-            // TODO
+            _function_queue.clear();
+            _function_queue.stop_wait();
+            if (_thread.joinable()) {
+                _thread.join();
+            }
         }
 
-        template<typename Function>
-            void setTimeout(Function function, int delay);
+        void setTimeout(std::function<void()> function, int delay) {
+            if (!_function_queue.push({ function, delay })) {
+                //
+            }
+        }
 
-            template<typename Function>
-            void setInterval(Function function, int interval);
+        void setInterval(DelayedFunction function, int interval) {
+            //
+        }
 
-        void                start(int64_t msec);
-        void                restart();
-        void                stop();
-
-
-        int64_t             interval() const;
-        bool                isActive() const;
-        bool                isSingleShot() const;
-        int64_t             remainingTime() const;
-        void                setInterval(int msec);
-        void                setSingleShot(bool singleShot);
+//        void                start(int64_t msec);
+//        void                restart();
+//        void                stop();
+//        int64_t             interval() const;
+//        bool                isActive() const;
+//        bool                isSingleShot() const;
+//        int64_t             remainingTime() const;
+//        void                setInterval(int msec);
+//        void                setSingleShot(bool singleShot);
 
     private:
 
-        FunctionType        _function;
+        using FunctionsQueue = AsyncQueue<DelayedFunction>;
+
+        FunctionsQueue      _function_queue;
+        std::thread         _thread;
 
     };
 
